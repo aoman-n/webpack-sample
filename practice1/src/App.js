e0b99c85e9d2f29225445db12b30a3a8c8f478c1
aoba <ipbdipbd00@gmail.com>
2018-11-28 23:40:20 +0900

    fix

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -5,16 +5,6 @@ import List from './List';
 import Header from './Header';
 import ebi from './images/ebi.jpg';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div styleName='root'>
-//         <h1>Hello Webpack!!testseee</h1>
-//       </div>
-//     )
-//   }
-// }
-
 function App() {
   return (
     <div>
